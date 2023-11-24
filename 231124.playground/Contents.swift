1f1462efc25b59c2dd5e20225469adfce4f1eb90
dahil4 <arhaxoda@naver.com>
2023-11-24 20:23:07 +0900

    chapter2

--- 231124.playground/Contents.swift
+++ 231124.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//옵셔널의 묵시적 해제는 ? 연산자 대신 ! 연산자만 붙여주면 된다.
+
+//명시적 옵셔널
+var str: String? = "swift"
+print(str)
+
+//묵시적 옵셔널
+var str2: String! = "swift"
+print(str2)
---
+++ 231124P/231124P/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  231124P
+//
+//  Created by A on 2023/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
