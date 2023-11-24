6ea9f380dfe8267ff3f4b4c655aaf0e383646035
dahil4 <arhaxoda@naver.com>
2023-11-24 20:36:33 +0900

    ss

--- 231124.playground/Contents.swift
+++ 231124.playground/Contents.swift
@@ -9,3 +9,13 @@ print(str)
 //묵시적 옵셔널
 var str2: String! = "swift"
 print(str2)
+
+
+//nil을 대입해도 문제가 없다!
+var str3: String! = nil
+
+//var value01: Int? = 10
+//value01 + 5
+
+var value02: Int! = 10
+value02 + 5
--- 231124P/231124P/ViewController.swift
+++ 231124P/231124P/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapButton(_ sender: Any) {
+        print("방가방가")
+        view.backgroundColor = .yellow
+        helloLabel.text = "하이"
+    }
+    
 }
 
