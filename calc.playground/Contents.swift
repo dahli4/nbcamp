32992a126b134b672dc0aad05a17164fdc5711ae
dahli4 <57888678+dahli4@users.noreply.github.com>
2023-12-01 21:00:15 +0900

    update

--- calc.playground/Contents.swift
+++ calc.playground/Contents.swift
@@ -1,36 +1,45 @@
 import UIKit
 
+class AbstractOperation {
+    func operate(_ num1: Int, _ num2: Int) -> Double? {
+        return nil
+    }
+}
 class Calculator{
-    var result: Double = 0.0
-    var history: [Double] = []
-    func oper(_ operation: (Double, Double) -> Double, _ num1: Double, _ num2: Double) -> Double {
-        result = operation(num1, num2)
-        history.append(result)
-        return result
+    private var abstractOperation: AbstractOperation
+    
+    init(abstractOperation: AbstractOperation) {
+        self.abstractOperation = abstractOperation
+    }
+    func setOperation(operation: AbstractOperation){
+        self.abstractOperation = operation
+    }
+    func calculate(num1: Int, num2: Int) -> Double{
+       abstractOperation.operate(num1, num2) ?? 0
     }
 }
 
-class AddOperation{
-    func add(_ num1:Double, _ num2:Double) -> Double{
-        return num1+num2
+class AddOperation: AbstractOperation{
+    override func operate(_ num1:Int, _ num2:Int) -> Double{
+        return Double(num1+num2)
     }
 }
 
-class SubtractOperation{
-    func subtract(_ num1:Double, _ num2:Double) -> Double{
-        return num1-num2
+class SubtractOperation: AbstractOperation{
+    override func operate(_ num1:Int, _ num2:Int) -> Double{
+        return Double(num1-num2)
     }
 }
 
-class MultiplyOperation{
-    func multiply(_ num1:Double, _ num2:Double) -> Double{
-        return num1*num2
+class MultiplyOperation: AbstractOperation{
+    override func operate(_ num1:Int, _ num2:Int) -> Double{
+        return Double(num1*num2)
     }
 }
-class DivideOperation{
-    func divide(_ num1:Double, _ num2:Double) -> Double{
+class DivideOperation: AbstractOperation{
+    override func operate(_ num1:Int, _ num2:Int) -> Double{
         if num2 != 0{
-            return num1/num2
+            return Double(num1/num2)
             
         }
         else{
@@ -38,20 +47,21 @@ class DivideOperation{
         }
     }
 }
-var cal = Calculator()
-var add = AddOperation()
-var sub = SubtractOperation()
-var mul = MultiplyOperation()
-var div = DivideOperation()
 
-var addResult = cal.oper(add.add, 30, 403)
-var subResult = cal.oper(sub.subtract, 40, 10)
-var mulResult = cal.oper(mul.multiply, -20, -7)
-var divResult = cal.oper(div.divide, 99, 9)
-var history = cal.history
+let calculator = Calculator(abstractOperation: AddOperation())
+
+let addResult = calculator.calculate(num1: 10, num2: 20)
+
+calculator.setOperation(operation: SubtractOperation())
+let subResult = calculator.calculate(num1: 40, num2: 10)
+
+calculator.setOperation(operation: MultiplyOperation())
+let mulResult = calculator.calculate(num1: 3, num2: 9)
+
+calculator.setOperation(operation: DivideOperation())
+let divResult = calculator.calculate(num1: 9, num2: 3)
 
 print("덧셈: \(addResult)")
 print("뺄셈: \(subResult)")
 print("곱셈: \(mulResult)")
 print("나눗셈: \(divResult)")
-print(history)
--- newCal.playground/Contents.swift
+++ newCal.playground/Contents.swift
@@ -1,3 +1,75 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class AddOperation {
+    func operate(num1: Int, num2: Int) -> Double {
+        return Double(num1 + num2)
+    }
+}
+
+class SubtractOperation {
+    func operate(num1: Int, num2: Int) -> Double {
+        return Double(num1 - num2)
+    }
+}
+
+class MultiplyOperation {
+    func operate(num1: Int, num2: Int) -> Double {
+        return Double(num1 * num2)
+    }
+}
+
+class DivideOperation {
+    func operate(num1: Int, num2: Int) -> Double {
+        return Double(num1 / num2)
+    }
+}
+
+class Calculator {
+    private let addOperation: AddOperation
+    private let subOperation: SubtractOperation
+    private let mulOperation: MultiplyOperation
+    private let divOperation: DivideOperation
+
+    init(addOperation: AddOperation,
+         subOperation: SubtractOperation,
+         mulOperation: MultiplyOperation,
+         divOperation: DivideOperation) {
+        self.addOperation = addOperation
+        self.subOperation = subOperation
+        self.mulOperation = mulOperation
+        self.divOperation = divOperation
+    }
+
+    func calculate(operator: String, num1: Int, num2: Int) -> Double? {
+        if `operator` == "+" {
+            return addOperation.operate(num1: num1, num2: num2)
+        }
+        if `operator` == "-" {
+            return subOperation.operate(num1: num1, num2: num2)
+        }
+        if `operator` == "*" {
+            return mulOperation.operate(num1: num1, num2: num2)
+        }
+        if `operator` == "/" {
+            return divOperation.operate(num1: num1, num2: num2)
+        }
+        return nil
+    }
+}
+
+let calculator = Calculator(
+    addOperation: AddOperation(),
+    subOperation: SubtractOperation(),
+    mulOperation: MultiplyOperation(),
+    divOperation: DivideOperation()
+)
+
+let addResult = calculator.calculate(operator: "+", num1: 10, num2: 20) ?? 0
+let subResult = calculator.calculate(operator: "-", num1: 49, num2: 29) ?? 0
+let mulResult = calculator.calculate(operator: "*", num1: 20, num2: 10) ?? 0
+let divResult = calculator.calculate(operator: "/", num1: 10, num2: 2) ?? 0
+
+print("Addition Result: \(addResult)")
+print("Subtraction Result: \(subResult)")
+print("Multiplication Result: \(mulResult)")
+print("Division Result: \(divResult)")
